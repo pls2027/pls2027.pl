342f0dd51e130b86ae85b750b8c35966cd8a4a47
pls2027 <polskaloteriasejmowa@gmail.com>
2026-07-03 15:00:04 +0200

    restore to old
    wracam

--- astro.config.mjs
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'astro/config';
-import tailwindcss from '@tailwindcss/vite';
-
-export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()],
-  },
-  site: 'https://pls2027.pl',
-});
